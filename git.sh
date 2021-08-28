5ae3e91969bc41d9acd785e6d38e1e4cf4dc94eb
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-08-28 16:25:22 +0100

    jq output github issues
    ref https://earthly.dev/blog/jq-select/
    
    ref https://github.com/stedolan/jq/issues/250
    
    ref https://stackoverflow.com/questions/46911763/jq-error-number-and-string-cannot-be-added
    
    ref https://stackoverflow.com/questions/37710718/concat-2-fields-in-json-using-jq

--- git.sh
+++ git.sh
@@ -28,7 +28,7 @@ function GitHubWorkoutRepoName {
 
 function listIssues {
   echo Listing issues from $GITHOST
-  curl -u $GITHUB_USERNAME:$GITHUB_ACCESS_TOKEN https://api.github.com/repos/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME/issues
+  curl --silent -u $GITHUB_USERNAME:$GITHUB_ACCESS_TOKEN https://api.github.com/repos/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME/issues | jq -r '.[] | (.number|tostring) + ":" + .title'
 }
 
 function getlistcommand {
