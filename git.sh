98669cad04609a78f3fc5fd699fe5f4b730b6193
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-08-30 09:41:27 +0100

    remove debugging

--- git.sh
+++ git.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -x
-
 function workoutGitHost {
   # Work out which git host this repository is using
   # e.g. is it github, gitlab, bare, bitbucket etc
@@ -27,7 +25,6 @@ function GitHubWorkoutRepoName {
 }
 
 function listIssues {
-  echo Listing issues from $GITHOST
   if [ "$GITHOST" = "github" ]; then
     curl -s -u $GITHUB_USERNAME:$GITHUB_ACCESS_TOKEN https://api.github.com/repos/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME/issues | jq -r '.[] | (.number|tostring) + ":" + .title'
 
@@ -37,14 +34,12 @@ function listIssues {
 function getListCommand {
   # Work out what they want to list
   #(issues/pull requests? etc)
-  echo You want to list $2 from $GITHOST
   if [ "$2" = "issues" ]; then
     listIssues $@
   fi
 }
 
 function readIssue {
-  echo Getting issue $3
   if [ "$GITHOST" = "github" ]; then
     curl --silent -u $GITHUB_USERNAME:$GITHUB_ACCESS_TOKEN https://api.github.com/repos/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME/issues/$3 | jq -r '.body'
   fi
@@ -53,14 +48,12 @@ function readIssue {
 function getReadCommand {
   # Work out what they want to read
   #(issues/pull requests? etc)
-  echo You want to read $2 from $GITHOST
   if [ "$2" = "issue" ]; then
     readIssue $@
   fi
 }
 
 function createIssue {
-  echo Creating issue $3
   echo Enter issue title:
   read issueTitle
   echo Enter issue description:
@@ -79,7 +72,6 @@ function createIssue {
 function getCreateCommand {
   # Work out what they want to create
   # e.g new issue
-  echo You want to create $2 from $GITHOST
   if [ "$2" = "issue" ]; then
     createIssue $@
   fi
@@ -98,7 +90,6 @@ function closeIssue {
 function getCloseCommand {
   # Work out what they want to close
   # e.g close issue
-  echo You want to close $2 from $GITHOST
   if [ "$2" = "issue" ]; then
     closeIssue $@
   fi
@@ -111,37 +102,28 @@ function getCommand {
   # otherwise, check if it's a command we've added
   # (e.g. like git list issues)
   exitCode=$?
-  echo The exit code was $exitCode
   if [ $exitCode -gt 2 ]; then
     exit $? # Was probably a error from git, dont continue
   fi
-  echo "working out what you want to do $1"
   
   if [ "$1" = "list" ]; then
-    echo You want to list something
     getListCommand $@
   fi
 
   if [ "$1" = "read" ]; then
-    echo You want to read something
     getReadCommand $@
   fi
 
   if [ "$1" = "new" ] || [ "$1" = "create" ]; then
-    echo "You want to create something new"
     getCreateCommand $@
   fi
 
   if [ "$1" = "close" ]; then
-    echo "You want to close something"
     getCloseCommand $@
   fi
 }
 
 function mygit {
-  echo This is not really git
-  echo $0
-  echo ${FUNCNAME}
   workoutGitHost # First work out which git host
   git $@ || getCommand $@
 }
